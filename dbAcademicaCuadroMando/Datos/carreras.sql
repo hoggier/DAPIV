072e2648546a95e928e1b94f6e10fabf81e9b979
Hoggier Cuellar <hrcuellar13@gmail.com>
2012-10-07 23:46:17 -0400

    560 materias agregadas y asignadas a las carreras terminando en la carrera 9

---
+++ dbAcademicaCuadroMando/Datos/carreras.sql
@@ -0,0 +1,18 @@
+﻿insert into carrera values
+(1,'Administración General',1),
+(2,'Administración de Turismo',1),
+(3,'Ingeniería Comercial',1),
+(4,'Ingeniería en Marketing y Publicidad',1),
+(5,'Auditoría Financiera',1),
+(6,'Administración Financiera',1),
+
+(7,'Ingeniería Industrial y Comercial',2),
+(8,'Ingeniería en Administracion Petrolera',2),
+(9,'Ingeniería Electronica y Sistemas',2),
+(10,'Ingeniería Mecánica Automotríz y Agroindustrial',2),
+(11,'Ingeniería de Sistemas',2),
+(12,'Ingeniería en Redes y Telecomunicaciones',2),
+
+(13,'Derecho',3),
+(14,'Relaciones Internacionales',3),
+(15,'Comercio Internacional',3)
---
+++ dbAcademicaCuadroMando/Datos/facultad.sql
@@ -0,0 +1,6 @@
+﻿insert into universidad values(1,'Utepsa')
+go
+insert into facultad values
+(1,'Cienias Empresariales',1),
+(2,'Cienias y Tecnología',1),
+(3,'Cienias Juridicas y Sociales',1)
